bc979fba3356427aa6c6544a213de28b54378a39
Yakovlev Andrew <drozy@yandex.ru>
2023-07-21 22:27:17 +0300

    Добавлены лог, триггеры

--- lesson6/sem6_hw_add.sql
+++ lesson6/sem6_hw_add.sql
@@ -128,3 +128,43 @@ SELECT hello();
 -- Создайте таблицу logs типа Archive. Пусть при каждом создании записи в таблицах users, communities и messages 
 -- в таблицу logs помещается время и дата создания записи, название таблицы, идентификатор первичного ключа. (Триггеры)
 
+-- Создание таблицы logs
+
+DROP TABLE IF EXISTS logs ;
+CREATE TABLE logs (
+id INT AUTO_INCREMENT PRIMARY KEY,
+table_name VARCHAR(255) NOT NULL,
+primary_key_id INT NOT NULL,
+created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Создание триггера для таблицы users
+DELIMITER $$
+CREATE TRIGGER users_insert_trigger AFTER INSERT ON users
+FOR EACH ROW
+BEGIN
+INSERT INTO logs (primary_key_id, table_name) VALUES (NEW.id, 'users');
+END$$
+DELIMITER ;
+
+-- Создание триггера для таблицы communities
+
+DELIMITER $$
+CREATE TRIGGER communities_insert_trigger AFTER INSERT ON communities
+FOR EACH ROW
+BEGIN
+INSERT INTO logs (primary_key_id, table_name) VALUES (NEW.id, 'communities');
+END$$
+DELIMITER ;
+
+-- Создание триггера для таблицы messages
+
+DELIMITER $$
+CREATE TRIGGER messages_insert_trigger AFTER INSERT ON messages
+FOR EACH ROW
+BEGIN
+INSERT INTO logs (primary_key_id, table_name) VALUES (NEW.id, 'messages');
+END$$
+DELIMITER ;
+
+SELECT * FROM logs;
